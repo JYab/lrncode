3753f632bba5caced667f277abf555b2fa1fde6a
JYab <jyabextra@gmail.com>
2017-05-29 08:11:02 -0400

    added to lrncode

--- learncpp/es1_5_1.cpp
+++ learncpp/es1_5_1.cpp
@@ -1,33 +1,26 @@
 #include <iostream>
 #include "sales_item.h"
+
 int main()
-{	
-	sales_item currItem, valItem;
-	if (std::cin >> currItem)
-	{
-		int cnt = 1;
-		while (std::cin >> valItem)
-		{
-			if (valItem.isbn() == currItem.isbn())
-			{
-				++cnt;
-			}
-
-			else 
-			{
-				std::cout << currItem << "occurs" 
-				<< cnt << "times" << std::endl;
-				currItem = valItem;
-				cnt = 1;
-			}
-	
-		}
-	
-	std::cout << currItem << "occurs" 
-	<< cnt << "times" << std::endl;
-
-	}
-	
-	return 0;
-
+{
+    Sales_item currItem, valItem;
+    if (std::cin >> currItem)
+    {
+        int cnt = 1;
+        while (std::cin >> valItem)
+        {
+            if (valItem.isbn() == currItem.isbn())
+            {
+                ++cnt;
+            }
+            else
+            {
+                std::cout << currItem << " occurs " << cnt << " times " << std::endl;
+                currItem = valItem;
+                cnt = 1;
+            }
+        }
+        std::cout << currItem << " occurs "<< cnt << " times " << std::endl;
+    }
+    return 0;
 }
